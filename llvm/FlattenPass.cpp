69699dee458486b93118cd985e51dcadf5a6890d
Psychotropos <truthbit2@gmail.com>
2018-02-04 19:22:44 +0000

    llvm: Properly implement flatten pass

--- llvm/FlattenPass.cpp
+++ llvm/FlattenPass.cpp
@@ -8,6 +8,7 @@
 #include "llvm/IR/Constants.h"
 #include "llvm/Transforms/Utils/Local.h"
 #include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Analysis/CallGraph.h"
 #include "MergeCallsPass.h"
 
 using namespace llvm;
@@ -15,9 +16,10 @@ using namespace llvm;
 namespace {
 struct Flatten : public FunctionPass {
   static char ID;
+  std::vector<Function*> functionsFromMain;
   Flatten() : FunctionPass(ID) {}
 
-  bool runOnFunction(Function &F) override {
+  bool inlineFunction(Function &F) {
       if(F.hasFnAttribute(Attribute::NoInline)) {
           // Function not eligible for inlining. Bail out.
           return false;
@@ -39,9 +41,9 @@ struct Flatten : public FunctionPass {
           CallInst* caller = cast<CallInst>(U);
           Function* parentFunction = caller->getFunction();
 
-          if(!parentFunction->hasFnAttribute("shellvm-main")) {
-              // Caller is not our main function, do not inline.
-              continue;
+          if(std::find(functionsFromMain.begin(), functionsFromMain.end(), parentFunction) != functionsFromMain.end()) {
+              // Called indirectly by main. This is not (currently) eligible.
+              return false;
           }
 
           callers.push_back(caller);
@@ -55,12 +57,65 @@ struct Flatten : public FunctionPass {
       // First, run the MergeCalls pass on it:
       MergeCalls* mergeCalls = new MergeCalls();
       mergeCalls->runOnFunction(F);
+      delete mergeCalls;
 
       // Coerce LLVM to inline the function.
       InlineFunctionInfo IFI;
       for(CallInst* caller : callers) {
           InlineFunction(caller, IFI, nullptr, true);
       }
+
+      // Check if we still have any users left. If not, we can proceed with erasing the function from the module.
+      bool hasUsers = false;
+      for(User* U : F.users()) {
+          hasUsers = true;
+          break;
+      }
+
+      if(!hasUsers) {
+          F.eraseFromParent();
+      }
+
+      return true;
+  }
+
+  void dfsTraverse(CallGraphNode* node) {
+      // Depth-first search of the call graph.
+      // Used to build the list of functions invoked (directly or indirectly) by the main routine.
+      for(auto &KV : *node) {
+          CallGraphNode* child = KV.second;
+          if(std::find(functionsFromMain.begin(), functionsFromMain.end(), child->getFunction()) == functionsFromMain.end()) {
+              functionsFromMain.push_back(child->getFunction());
+          }
+
+          dfsTraverse(child);
+      }
+  }
+
+  bool runOnFunction(Function &F) override {
+      if(!F.hasFnAttribute("shellvm-main")) {
+          // We only want to run on the main function.
+          return false;
+      }
+
+      // Build the call graph of the entire module.
+      CallGraph* cg = new CallGraph(*F.getParent());
+
+      // Start traversing the call graph for the main function.
+      CallGraphNode* cgn = cg->operator[](&F);
+      dfsTraverse(cgn);
+
+      do {
+          // Kinda messy, but necessary in this case. We need to signal deletion of any inlined functions from functionsFromMain.
+          // This requires us to alter functionsFromMain from within the context of the loop.
+          for(std::vector<Function*>::iterator it = functionsFromMain.begin(); it != functionsFromMain.end(); ) {
+              if(inlineFunction(*(*it))) {
+                  it = functionsFromMain.erase(it);
+              } else {
+                  ++it;
+              }
+          }
+      } while(functionsFromMain.size() > 0);
 
       return true;
   }
